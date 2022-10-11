16ed744248229a05a5bdfea6408ed73fc502833a
jmb05 <jmb05@web.de>
2022-10-11 17:17:37 +0200

    Removed Tooltip

--- src/main/java/net/jmb19905/lessglintythings/ModConfig.java
+++ src/main/java/net/jmb19905/lessglintythings/ModConfig.java
@@ -2,12 +2,9 @@ package net.jmb19905.lessglintythings;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = LessGlintyThings.MOD_ID)
 public class ModConfig implements ConfigData {
-    @ConfigEntry.Gui.Tooltip
     public boolean removeBookGlint = true;
-    @ConfigEntry.Gui.Tooltip
     public boolean removePotionGlint = false;
 }
